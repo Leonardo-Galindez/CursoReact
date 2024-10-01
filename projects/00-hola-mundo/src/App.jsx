64ad7468af625ee80fb9a1c793fca432727323d6
Leonardo Galindez <88455461+Leonardo-Galindez@users.noreply.github.com>
2024-09-30 21:27:52 -0300

    ejemplos de useState

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,36 +1,48 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
-
+import { useState } from 'react' //hooks o utilidad
 export function App() {
     const format = (userName) => `@${userName}`
     //creamos el objeto user
     const user1 = { isFollowing: true, userName: 'midudev' }
     //mandar como objeto a el componenete como promps
+    const [nombre, setName] = useState('midudev')
+    console.log('nombre', nombre)
+
     return (
+
         // <> ó React.Fragment
         //isFollowing={true} o isFollowing
         //mandar funciones como parametros o props
+        
+        //render
         <section className='App'>
             <TwitterFollowCard
                 formatUserName={format}
-                isFollowing 
+                isFollowing
                 userName="midudev"
                 name="Leonardo David Galindez"
             />
+            {/*comentario en el render*/}
             <TwitterFollowCard
                 formatUserName={format}
-                isFollowing={false} userName="galiLeonard"
-                name="Leonardo David Galindez"
+                isFollowing={false} userName={nombre}
+                name={nombre}
             />
             <TwitterFollowCard
                 formatUserName={format}
-                isFollowing={true} 
-                name="Leonardo David Galindez"
+                isFollowing={true}
+                name={nombre}
             />
 
             <TwitterFollowCard formatUserName={format} isFollowing={false} userName="fede">
                 <strong>Leonardo David Galindez</strong>
-            </TwitterFollowCard>       
+            </TwitterFollowCard>
+
+            <button onClick={() => setName('leonardo')}>
+                Cambio Nombre
+            </button>
+
         </section>
     )
 }
